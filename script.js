3d875ef707082e3e7258fb10aff9a1d48679a3af
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-07 13:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+	this.make = make;
+	this.model = model;
 }
-Car.prototype.getModel=function(){
+
+Car.prototype.getModel = function() {
 	return `${this.make} and ${this.model}`;
-}
+};
+
+Car.prototype.getMakeModel = function() {
+	return `${this.make} ${this.model}`;
+};
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed=topSpeed;
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
-SportsCar.prototype.getTopSpeed=function () {
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
 	return this.topSpeed;
-}
-
-
+};
 
 // Do not change the code below
 window.Car = Car;
